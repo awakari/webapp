6496a6f6f86ca20d33f6ac8ab1e32e766235c72b
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-08-25 09:07:02 +0300

    save

--- assets/subs.js
+++ assets/subs.js
@@ -7,4 +7,17 @@ function subsLoad() {
         },
         cache: "default",
     }
+    fetch("/v1/subscriptions", optsReq)
+        .then(resp => {
+            if (!resp.ok) {
+                throw new Error(`Request failed with status: ${resp.status}`);
+            }
+            return resp.json();
+        })
+        .then(data => {
+            console.log("Response data:", data);
+        })
+        .catch(err => {
+            console.error("Error: ", err)
+        })
 }

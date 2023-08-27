51346651a424aaf40624bd619aadcf2d7cdf3e6b
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-08-27 10:00:08 +0300

    save

--- assets/inbox.js
+++ assets/inbox.js
@@ -2,9 +2,7 @@ const templateInboxNav = subId => `
             <p class="text-lg"><b>Subscription Inbox</b></p>
             <span class="flex space-x-2" style="margin-left: auto; margin-right: 0">
                 <button type="button" title="Refresh" onclick="loadNewMsgs('${subId}')" class="h-8 w-8 text-sm focus:outline-none text-blue-500 rounded-md border border-blue-500 hover:bg-blue-200 flex items-center">
-                    <svg class="fill-current w-4 h-4 mx-auto" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512">
-                        <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"/>
-                    </svg>
+                    <img src="/web/inbox-reload.svg" alt="Refresh" class="px-1"/>
                 </button>
                 <a href="/web/sub-edit.html?id=${subId}">
                     <button title="Edit Subscription" class="h-8 w-8 rounded-md border border-indigo-700 shadow-2xl hover:bg-indigo-200 text-indigo-700 items-center">

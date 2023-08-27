56acfcc07b20990c45ae033e70a40378c947529f
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-08-27 14:21:34 +0300

    save

--- assets/msg-view.js
+++ assets/msg-view.js
@@ -9,9 +9,7 @@ function loadEvent() {
             document.getElementById("evt_source").innerHTML = `<a href="${evt.source}">${evt.source}</a>`;
             document.getElementById("evt_time").innerText = new Date(evt.attributes.time.Attr.CeTimestamp.seconds * 1000).toISOString();
             document.getElementById("evt_text_data").innerText = evt.Data != null && evt.Data.TextData != null ? evt.Data.TextData : "";
-            for (const attrKey of evt.attributes) {
-                console.log(attrKey, evt.attributes[attrKey]);
-            }
+            document.getElementById("evt_ext_attrs").innerText = JSON.stringify(evt.attributes)
         }
     })
 }

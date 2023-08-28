98a206724c4ad545fd8ba101097c4ff5aa6c5fc6
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-08-28 21:53:04 +0300

    save

--- assets/msg-view.js
+++ assets/msg-view.js
@@ -8,7 +8,7 @@ function loadEvent() {
             evt.read = true
             document.getElementById("evt_id").innerText = evt.id;
             document.getElementById("evt_source").innerHTML = `<a href="${evt.source}">${evt.source}</a>`;
-            document.getElementById("evt_time").innerText = new Date(evt.attributes.time.ce_timestamp.seconds * 1000).toISOString();
+            document.getElementById("evt_time").innerText = evt.attributes.time.ce_timestamp;
             document.getElementById("evt_title").innerText = evt.attributes.title != null ? evt.attributes.title.ce_string : "";
             document.getElementById("evt_summary").innerText = evt.attributes.summary != null ? evt.attributes.summary.ce_string : "";
             document.getElementById("evt_text_data").innerText = evt.text_data != null ? evt.text_data : "";

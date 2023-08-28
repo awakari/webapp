62b68342785288957b359507b894575316d70f38
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-08-28 08:34:58 +0300

    save

--- assets/inbox.js
+++ assets/inbox.js
@@ -2,7 +2,7 @@ const templateInboxNav = subId => `
             <p class="text-lg"><b>Subscription Inbox</b></p>
             <span class="flex space-x-2" style="margin-left: auto; margin-right: 0">
                 <button type="button" title="Refresh" onclick="loadEvents('${subId}')" class="h-8 w-8 text-sm focus:outline-none text-blue-500 rounded-md border border-blue-500 hover:bg-blue-200 flex items-center">
-                    <img src="/web/inbox-reload.svg" alt="Refresh" class="px-1"/>
+                    <img src="/web/reload.svg" alt="Refresh" class="px-1"/>
                 </button>
                 <button title="Edit Subscription" onclick="window.location.assign('/web/sub-edit.html?id=${subId}')" class="h-8 w-8 rounded-md border border-indigo-700 shadow-2xl hover:bg-indigo-200 text-indigo-700 items-center">
                     <img src="/web/sub-edit.svg" alt="Edit Subscription" class="px-1"/>

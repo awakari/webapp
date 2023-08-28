b7b0654e7fd81c284595b9ba1931023da65bec24
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-08-28 09:17:41 +0300

    save

--- assets/subs.js
+++ assets/subs.js
@@ -9,7 +9,7 @@ const templateSub = (sub, unreadEvtsCount) => `
                             </span>
                             <span class="flex space-x-2">
                                 <p class="truncate w-[256px] ${sub.data.hasOwnProperty("enabled") && sub.data.enabled ? "" : "text-slate-500"}">
-                                    <span class="bg-indigo-600 h-4 w-4 flex justify-center items-center text-white rounded-full" style="visibility: ${unreadEvtsCount > 0 ? "inherit" : "none"}">
+                                    <span class="absolute -top-2 -right-2 bg-indigo-600 h-3 w-3 flex justify-center items-center text-white text-xs rounded-full" style="display: ${unreadEvtsCount > 0 ? "inherit" : "none"}">
                                         ${unreadEvtsCount}
                                     </span>
                                     ${sub.data.description}

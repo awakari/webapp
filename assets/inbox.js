fe61a1e4e406ecdf42e35427c6e90753ea19fb8a
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-09-05 11:48:11 +0300

    + new events long polling on the subs page

--- assets/inbox.js
+++ assets/inbox.js
@@ -59,7 +59,7 @@ function loadInboxNav(subId) {
 function loadInboxEvents(subId) {
     let evtsHistory = Events.GetLocalHistory(subId);
     Events
-        .Load(subId, evtsHistory, 100)
+        .Load(subId, evtsHistory, 1_000)
         .finally(_ => {
             displayEvents(subId, evtsHistory)
         })

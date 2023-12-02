71fdd1064c5fa5bc856f132fc46a7b660cfe68b9
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-12-02 09:37:30 +0200

    save

--- web/subs.js
+++ web/subs.js
@@ -5,7 +5,9 @@ const templateSub = (sub) => `
                     </span>
                     <div class="grow"></div>
                     <span>
-                        🠆                                         
+                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M4 12H20M20 12L16 8M20 12L16 16" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>                                         
                     </span>
                 </div>
 `

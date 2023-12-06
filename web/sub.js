c33f79759b6bd5798fca5fe6cae597d9787c213b
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-12-06 13:13:57 +0200

    test

--- web/sub.js
+++ web/sub.js
@@ -4,7 +4,6 @@ const templateSub = (sub) => `
                     <span class="truncate w-[240px] sm:w-[600px] py-2">
                         ${sub.description}
                     </span>
-                    <div class="grow max-"></div>
                     <span>
                         <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M4 12H20M20 12L16 8M20 12L16 16" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>

d105bf517c974afaa9c7fe0a32f706c9e104702f
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-12-07 09:16:34 +0200

    save

--- web/sub-details.js
+++ web/sub-details.js
@@ -23,36 +23,36 @@ function loadSubscription() {
     const id = urlParams.get("id");
     document.getElementById("id").value = id;
     //
-    // const data = {"description":"Exoplanets","enabled":true,"cond":{"gc":{"logic":"Or","group":[{"tc":{"id":"txt_651f009c25fef58d2c176c06","term":"exoplanet экзопланета экзопланет экзопланеты экзопланету"}},{"gc":{"group":[{"tc":{"id":"txt_651f009c25fef58d2c176c13","term":"planet"}},{"tc":{"id":"txt_651f009c25fef58d2c176c27","term":"extrasolar"}}]}}]}},"expires":"0001-01-01T00:00:00Z"};
-    // editor.setValue(data.cond);
+    const data = {"description":"Exoplanets","enabled":true,"cond":{"gc":{"logic":"Or","group":[{"tc":{"id":"txt_651f009c25fef58d2c176c06","term":"exoplanet экзопланета экзопланет экзопланеты экзопланету"}},{"gc":{"group":[{"tc":{"id":"txt_651f009c25fef58d2c176c13","term":"planet"}},{"tc":{"id":"txt_651f009c25fef58d2c176c27","term":"extrasolar"}}]}}]}},"expires":"0001-01-01T00:00:00Z"};
+    editor.setValue(data.cond);
     //
-    let authToken = sessionStorage.getItem("authToken");
-    let userId = sessionStorage.getItem("userId");
-    let optsReq = {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${authToken}`,
-            "X-Awakari-Group-Id": defaultGroupId,
-            "X-Awakari-User-Id": userId,
-        },
-        cache: "default",
-    }
-    fetch(`/v1/sub/${id}`, optsReq)
-        .then(resp => {
-            if (!resp.ok) {
-                throw new Error(`Request failed with status: ${resp.status}`);
-            }
-            return resp.json();
-        })
-        .then(data => {
-            if (data != null) {
-                document.getElementById("description").value = data.description;
-                editor.setValue(data.cond);
-            }
-        })
-        .catch(err => {
-            alert(err);
-        });
+    // let authToken = sessionStorage.getItem("authToken");
+    // let userId = sessionStorage.getItem("userId");
+    // let optsReq = {
+    //     method: "GET",
+    //     headers: {
+    //         "Authorization": `Bearer ${authToken}`,
+    //         "X-Awakari-Group-Id": defaultGroupId,
+    //         "X-Awakari-User-Id": userId,
+    //     },
+    //     cache: "default",
+    // }
+    // fetch(`/v1/sub/${id}`, optsReq)
+    //     .then(resp => {
+    //         if (!resp.ok) {
+    //             throw new Error(`Request failed with status: ${resp.status}`);
+    //         }
+    //         return resp.json();
+    //     })
+    //     .then(data => {
+    //         if (data != null) {
+    //             document.getElementById("description").value = data.description;
+    //             editor.setValue(data.cond);
+    //         }
+    //     })
+    //     .catch(err => {
+    //         alert(err);
+    //     });
 }
 
 function updateSubscription() {

a540b36f60ec3e0cfd384d3c042842e4ecfd9d94
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-12-07 19:24:49 +0200

    save

--- web/pub.js
+++ web/pub.js
@@ -1,3 +1,17 @@
+const templateSrc = (type, addr) => `
+                <div class="hover:text-blue-500 hover:bg-gray-100 flex"
+                     onclick="window.location.assign('pub-src-details.html?type=${type}&addr=${addr}')">
+                    <span class="truncate w-[240px] sm:w-[600px] py-2">
+                        ${addr}
+                    </span>
+                    <span>
+                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M4 12H20M20 12L16 8M20 12L16 16" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>                                         
+                    </span>
+                </div>
+`
+
 function load() {
 
     const authToken = sessionStorage.getItem("authToken");
@@ -43,17 +57,18 @@ function load() {
             if (data != null && data.hasOwnProperty("count")) {
                 document.getElementById("limit").innerText = data.count;
             }
-            return data.count;
+            return data;
         })
         .catch(err => {
             alert(err);
         })
 
-    loadSources();
+    loadSources("");
 }
 
-function loadSources() {
+function loadSources(cursor) {
 
+    const filter = document.getElementById("filter").value;
     const authToken = sessionStorage.getItem("authToken");
     const userId = sessionStorage.getItem("userId");
     const headers = {
@@ -61,6 +76,39 @@ function loadSources() {
         "X-Awakari-Group-Id": defaultGroupId,
         "X-Awakari-User-Id": userId,
     }
+    const srcType = document.getElementById("src_type");
+    const own = document.getElementById("own").checked;
 
-
+    fetch(`/v1/src/${srcType}?cursor=${cursor}&own=${own}&limit=10`, {
+        method: "GET",
+        headers: headers,
+        cache: "default",
+    })
+        .then(resp => {
+            if (!resp.ok) {
+                throw new Error(`Daily publishing usage request failed with status: ${resp.status}`);
+            }
+            return resp.json();
+        })
+        .then(data => {
+            if (data != null) {
+                let listHtml = document.getElementById("src_list");
+                for (const addr of data) {
+                    if (filter !== "") {
+                        const input  = [
+                            addr,
+                        ]
+                        const idxs = uf.filter(input, filter);
+                        if (idxs != null && idxs.length > 0) {
+                            listHtml.innerHTML += templateSub(srcType, addr);
+                        }
+                    } else {
+                        listHtml.innerHTML += templateSub(srcType, addr);
+                    }
+                }
+            }
+        })
+        .catch(err => {
+            alert(err);
+        })
 }

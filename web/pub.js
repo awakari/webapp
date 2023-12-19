1e13536a9744a357851b2eb13b83abca4e344315
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-12-19 15:53:59 +0200

    save

--- web/pub.js
+++ web/pub.js
@@ -4,7 +4,7 @@ const templateSrc = (type, addr) => `
                     <span class="truncate w-[240px] sm:w-[600px] py-2">
                         ${addr}
                     </span>
-                    <span>
+                    <span class="pt-1">
                         <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M6 12H18M18 12L13 7M18 12L13 17" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>                                         

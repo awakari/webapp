c26720c9116cf2ff36050b828745741654c36e73
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-12-20 11:03:43 +0200

    save

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,3 @@
-self.addEventListener("fetch", event => {
-    // This is a dummy event listener
-    // just to pass the PWA installation criteria on
-    // some browsers
+self.addEventListener("fetch", evt => {
+    console.log(evt);
 });

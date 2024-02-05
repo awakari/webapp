7433f3ea485a5ce0a2403d9ab0f8995cdbfc7aa8
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2024-02-05 09:07:49 +0200

    save

--- web/pub-src-details.js
+++ web/pub-src-details.js
@@ -40,11 +40,13 @@ function loadSource() {
                                 break
                         }
                         document.getElementById("addr").innerHTML = `<a href="${data.addr}" target="_blank" class="text-blue-500">${data.addr}</a>`;
+                        document.getElementById("freq-charts").display = "block";
                         break
                     case "site":
                         document.getElementById("type").innerText = "Site";
                         document.getElementById("upd_period").innerText = "Polling once a day";
                         document.getElementById("addr").innerHTML = `<a href="${data.addr}" target="_blank" class="text-blue-500">${data.addr}</a>`;
+                        document.getElementById("freq-charts").display = "none";
                         break
                     case "tgch":
                         document.getElementById("type").innerText = "Telegram channel (App)";
@@ -55,6 +57,7 @@ function loadSource() {
                         } else {
                             document.getElementById("addr").innerHTML = `<a href="${data.addr}" target="_blank" class="text-blue-500">${data.addr}</a>`;
                         }
+                        document.getElementById("freq-charts").display = "none";
                         break
                     case "tgbc":
                         document.getElementById("type").innerText = "Telegram channel (Bot)";
@@ -65,6 +68,7 @@ function loadSource() {
                         } else {
                             document.getElementById("addr").innerHTML = `<a href="${data.addr}" target="_blank" class="text-blue-500">${data.addr}</a>`;
                         }
+                        document.getElementById("freq-charts").display = "none";
                         break
                 }
                 if (data.lastUpdate === "0001-01-01T00:00:00Z") {

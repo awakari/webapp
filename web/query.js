18f5122fe4fa39514783a58866bd5f70a72d9363
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2024-02-29 21:50:34 +0200

    feat: draft new search ui

--- web/query.js
+++ web/query.js
@@ -1,13 +1,13 @@
 function queryType() {
     if (document.getElementById('query').value === '') {
-        document.getElementById('login').style.display='none';
+        queryStop();
     }
 }
 
 function querySubmit() {
     const q = document.getElementById('query').value;
     if (q === '') {
-        document.getElementById('login').style.display = 'none';
+        queryStop();
     } else {
         const userId = localStorage.getItem(keyUserId);
         switch (userId) {
@@ -95,5 +95,6 @@ async function startEventsLoading(subId) {
 }
 
 function queryStop() {
+    document.getElementById("events-menu").style.display = "none";
     alert("TODO: queryStop");
 }

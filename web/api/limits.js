2a2b37127320c2b552872eb6b153239a991d587e
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2024-03-25 22:59:36 +0200

    save [skip ci]

--- web/api/limits.js
+++ web/api/limits.js
@@ -8,15 +8,19 @@ Limits.fetch = function (subj, headers) {
     })
         .then(resp => handleCookieExpiration(resp, headers, (h) => Limits.fetch(subj, h)))
         .then(resp => {
-            if (!resp.ok) {
-                if (subj === "1" || subj === "2") {
-                    handleResponseStatus(resp.status);
-                } else {
-                    alert(`Invalid subject: ${subj}`);
+            if (resp) {
+                if (!resp.ok) {
+                    if (subj === "1" || subj === "2") {
+                        handleResponseStatus(resp.status);
+                    } else {
+                        alert(`Invalid subject: ${subj}`);
+                    }
+                    return null;
                 }
+                return resp.json();
+            } else {
                 return null;
             }
-            return resp.json();
         })
 
 }

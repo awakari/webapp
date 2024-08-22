1c5a4c354935efae3e0cd8f787c112a416f589c1
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2024-08-22 10:16:05 +0300

    fix: styles minor [skip ci]

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./web/*.{html,js}", "./web/articles/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['"IA Writer Quattro S"', 'ui-sans-serif', 'system-ui'],
+      'display': ['"IA Writer Quattro S"', 'ui-sans-serif', 'system-ui'],
+      'body': ['"IA Writer Quattro S"', 'ui-sans-serif', 'system-ui'],
+    }
   },
   plugins: [],
 }

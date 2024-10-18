263c3b8591d55a6380a237f2fd010e8441179946
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2024-10-18 15:07:53 +0300

    fix: new donation options [skip ci]

--- web/donation.js
+++ web/donation.js
@@ -1,12 +1,3 @@
 function openDonationPage() {
-    let link = "https://donate.stripe.com/14k7uCaYq5befN65kk";
-    const userId = localStorage.getItem(keyUserId);
-    if (userId) {
-        const customerId = userId.replace(/[^a-zA-Z0-9]/g, '_');
-        link += `?prefilled_email=${customerId}%40awakari.com`;
-    } else if (!confirm("You're not signed in. Donate anonymously?")) {
-        window.location.assign("login.html?redirect=donation.html");
-        return
-    }
-    window.location.assign(link);
+    window.location.assign("donation.html");
 }

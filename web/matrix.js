b7d527897e588666978fc9662036b54b183f3015
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2025-04-19 00:52:57 +0300

    fix: main page [skip ci]

--- web/matrix.js
+++ web/matrix.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("matrix");
 const context = canvas.getContext('2d');
 
-const alphabet = '0123456789aBcDeFgHiJkLmQZ▶@@@@$$$#ᕔᘎᘕᚦᚠᚱᛜᛝᛟ☑אقڜ✓±∑£₿₿₿₿∀∈¥∞∫≈≤≥⊃⊳Ⅺ¾½⅔㊷ðöëЫЙЯЮαβγδεζηθλμπρσω©༜?!⌲⌧⌧℃⎈⏵⏯⏾▱▽◇◆◊○☀☂☆♙♡♪♪♫♫♬♬♥♥♥⚙⚠⚛⛁⛅⛭✈✻➤§';
+const alphabet = '0123456789aBcDeFgHiJkLmQRsTUvWXyZ▶@@@@@@@@@@$$$$$#ᕔᘎᘕᚦᚠᚱᛜᛝᛟ☑אقڜ✓±∑£₿₿₿₿₿₿₿₿₿₿∀∈¥¥∞∫≈≤≥⊃⊳Ⅺ¾½⅔㊷ðöëЫЙЯЮαβγδεζηθλμπρσω©༜?!⌲⌧⌧⌧⌧⌧℃⎈⏵⏯⏾▱▽◇◆◊○☀☂☆♙♡♡♡♡♡♪♪♫♫♬♬♥♥⚙⚠⚛⛁⛭✈✻➤§';
 
 const fontSize = 20;
 const columns = canvas.width/fontSize;
@@ -26,7 +26,7 @@ const draw = () => {
     for(let i = 0; i < rainDrops.length; i++) {
         const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
         context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
-        if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.98){
+        if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.95){
             rainDrops[i] = 0;
         }
         rainDrops[i]++;

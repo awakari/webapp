64bde0692a612164e470c60d9b317000495496a8
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2025-04-19 00:32:27 +0300

    fix: main page [skip ci]

--- web/matrix.js
+++ web/matrix.js
@@ -1,9 +1,9 @@
 const canvas = document.getElementById("matrix");
 const context = canvas.getContext('2d');
 
-const alphabet = '0123456789aZ▶@$#सॠஔฬༀᕔᘎᘕᚦᚠᚱᛜᛝᛟ☑אقڜ✓±∑£₿∀∈¥∞∫≈≤≥⊃⊳Ⅺ¾½⅔㊷ðöëЫЙЯЮαβγδεζηθλμπρσω©༜?!⌲⌧⌛⌚℃⎈⏱⏳⏵⏯⏾▱▽◇◆◊○☀☂☆☘☺♁♉☿♆♏♙♡♪♫♬♥⚙⚠⚛⛁⛅⛭⛵✈✻➤§';
+const alphabet = '0123456789aBcDeFgHiJkLmQZ▶@@@@$$$#ᕔᘎᘕᚦᚠᚱᛜᛝᛟ☑אقڜ✓±∑£₿₿₿₿∀∈¥∞∫≈≤≥⊃⊳Ⅺ¾½⅔㊷ðöëЫЙЯЮαβγδεζηθλμπρσω©༜?!⌲⌧⌧℃⎈⏵⏯⏾▱▽◇◆◊○☀☂☆♙♡♪♪♫♫♬♬♥♥♥⚙⚠⚛⛁⛅⛭✈✻➤§';
 
-const fontSize = 16;
+const fontSize = 20;
 const columns = canvas.width/fontSize;
 
 const rainDrops = [];
@@ -15,22 +15,18 @@ const draw = () => {
 
     const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
     if (isDarkMode) {
-        context.fillStyle = 'rgba(17, 24, 39, 0.1)';
+        context.fillStyle = 'rgba(17, 24, 39, 0.2)';
     } else {
-        context.fillStyle = 'rgba(255, 255, 255, 0.1)';
+        context.fillStyle = 'rgba(255, 255, 255, 0.2)';
     }
     context.fillRect(0, 0, canvas.width, canvas.height);
-    if (isDarkMode) {
-        context.fillStyle = '#0c0';
-    } else {
-        context.fillStyle = '#090';
-    }
+    context.fillStyle = '#3b82f6';
     context.font = fontSize + 'px monospace';
 
     for(let i = 0; i < rainDrops.length; i++) {
         const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
         context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
-        if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+        if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.98){
             rainDrops[i] = 0;
         }
         rainDrops[i]++;
